7b8b6439bc528e74a407cb931df95a883a6483ad
null-91 <sophiea0110@naver.com>
2022-08-12 10:10:09 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.MemberRepository;
+import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    Member member;
+    MemberRepository mr;
+
+    @GetMapping("hello")
+    public String Hello(Model model){
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name, Model model){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
+}
---
+++ src/main/java/hello/hellospring/servie/MemberService.java
@@ -0,0 +1,37 @@
+package hello.hellospring.servie;
+
+import hello.hellospring.MemberRepository;
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+
+        validateDupliateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDupliateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
